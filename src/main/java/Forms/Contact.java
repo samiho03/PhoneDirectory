87cffe2b6479c4ac95f4aff52562eeb98aabef34
Sakuni Rajapaksha <131904311+samiho03@users.noreply.github.com>
2024-08-13 20:09:06 +0530

    First Commit

---
+++ src/main/java/Forms/Contact.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Forms;
+
+/**
+ *
+ * @author Dell
+ */
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String relation;
+    private String email;
+    private String category;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String relation, String email, String category) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.relation = relation;
+        this.email = email;
+        this.category = category;
+    }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    
+}
